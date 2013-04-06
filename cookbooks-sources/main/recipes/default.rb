d06d2d5ea684e74e5c5097b678f868c505b5e681
Alexander Korkin <av.korkin@gmail.com>
2013-04-06 18:12:17 +0400

    fixes

--- cookbooks-sources/main/recipes/default.rb
+++ cookbooks-sources/main/recipes/default.rb
@@ -7,5 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
+
 include_recipe "redisio::install"
 include_recipe "redisio::enable"
--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -6,32 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "git-core"
-package 'git'
-package 'python-software-properties'
-package 'curl'
-package "build-essential"
-
-#Redis and resque requarements 
-include_recipe "apt"
-include_recipe "ruby_build"
-include_recipe "rbenv::user"
-include_recipe "ulimit"
-
-package "curl"
-#include_recipe redisio::install"
-#include_recipe "redisio::enable"
-
-gem_package "ruby-shadow" do
-  action :install
-end
-gem_package "bundler"
 
 
-user node[:user][:name] do
-  password node[:user][:password]
-  gid "admin"
-  home "/home/#{node[:user][:name]}"
-  supports manage_home: true
-  shell "/bin/bash"
-end
+include_recipe "redisio::install"
+include_recipe "redisio::enable"
